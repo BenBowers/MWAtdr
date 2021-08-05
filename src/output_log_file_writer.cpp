02511d01c5d2512149c09584aef8bb285bbec590
Zak Booth <zak.booth@student.curtin.edu.au>
2021-08-05 13:49:18 +0800

    CP6-78 Output log file writer stub
    Added .cpp and .hpp files, added .cpp to make list.

---
+++ src/output_log_file_writer.cpp
@@ -0,0 +1,6 @@
+#include "output_log_file_writer.hpp"
+
+void writeLogFile(AppConfig observation, ChannelRemapping mapping,
+				  ObservationProcessingReults results, AntennaConfig physical) {
+    //TODO: write to file
+}
---
+++ src/output_log_file_writer.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+void writeLogFile(AppConfig observation, ChannelRemapping mapping,
+				  ObservationProcessingReults results, AntennaConfig physical);
