cf3323450c0486b50690f4b27d5ca1b79e8dcfa6
Zak Booth <zak.booth@student.curtin.edu.au>
2021-08-05 13:59:08 +0800

    CP6-78 Output log file writer stub
    Included channel_remapping.hpp to output_log_file_writer.hpp

--- src/output_log_file_writer.hpp
+++ src/output_log_file_writer.hpp
@@ -1,4 +1,10 @@
 #pragma once
 
+#include "common.hpp"
+#include "channel_remapping.hpp"
+
+#include <fstream>
+#include <iostream>
+
 void writeLogFile(AppConfig observation, ChannelRemapping mapping,
 				  ObservationProcessingReults results, AntennaConfig physical);
