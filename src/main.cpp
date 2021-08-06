6363d108643eaf38d19ababaa0692c8c92a8d264
Zak Booth <zak.booth@student.curtin.edu.au>
2021-08-06 17:13:47 +0800

    CP6-93 Metadata file reader stub
    Removed files to test functionality, and renamed .hpp to follow coding standard.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,3 @@
-#include "common.hpp"
-#include "metadata_file_reader.hpp"
-
-#include <iostream>
-
 int main() {
-	AntennaConfig antennas;
-	MetadataFileReader metareader;
 
-	antennas = metareader.getAntennaConfig();
-
-	for (AntennaInputPhysID i : antennas.antennaInputs)
-	{
-		std::cout << "Antenna " << i.tile << " " << i.signalChain << std::endl;
-	}
-
-	for (unsigned i : antennas.frequencyChannels)
-	{
-		std::cout << i << ", ";
-	}
-
-	std::cout << std::endl;
 }
