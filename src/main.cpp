4b4959a75e6ffc688452c38a110b8111a55bfe84
Zak Booth <zak.booth@student.curtin.edu.au>
2021-08-07 00:34:53 +0800

    CP6-87 Node antenna input assigner stub
    Removed test functionality from main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-#include "node_antenna_input_assigner.hpp"
-
-#include <iostream>
-
 int main() {
-	auto ranges = assignNodeAntennaInputs(4, 4);
 
-    for (auto const& i : ranges) {
-        std::cout << i.value().begin;
-	}
 }
