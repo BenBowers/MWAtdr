3e146e7b1791e4d15d85dfd23fbd858e124bec09
Zak Booth <zak.booth@student.curtin.edu.au>
2021-08-07 14:44:25 +0800

    CP6-93 Metadata file reader stub
    Changed struct to class. Updated make list

--- src/MetadataFileReader.hpp
+++ src/MetadataFileReader.hpp
@@ -1,20 +1,3 @@
 #pragma once
 
-#include "common.hpp"
-
-struct MetadataFileReader {
-	// TODO: Add constructor to build VoltageContext
-
-    AntennaConfig getAntennaConfig() {
-		AntennaConfig config;
-
-		// TODO: getPhysicalAntennaMappings()
-		AntennaInputPhysID arr[] = {{0, 'X'}, {0, 'Y'}, {1, 'X'}, {1, 'Y'},
-									{6, 'X'}, {6, 'Y'}, {7, 'X'}, {7, 'Y'}};
-		config.antennaInputs.assign(arr, arr + 8);
-		// TODO: getFrequencyChannelsUsed()
-		config.frequencyChannels.insert({124, 125, 127, 128, 129});
-
-		return config;
-	}
-};
+class MetadataFileReader;
