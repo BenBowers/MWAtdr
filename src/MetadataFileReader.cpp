b7ef3d7f199849d86f5400b1e236b0d5b5b0f15c
Zak Booth <zak.booth@student.curtin.edu.au>
2021-08-07 18:28:33 +0800

    CP6-93 Metadata file reader stub
    Added class definition to .hpp, adjusted .cpp to fix syntax

--- src/MetadataFileReader.cpp
+++ src/MetadataFileReader.cpp
@@ -1,20 +1,17 @@
-#include "Common.hpp"
 #include "MetadataFileReader.hpp"
 
-class MetadataFileReader {
-	public:
-		// TODO: Add constructor to build VoltageContext
+// TODO: Add constructor to build VoltageContext
 
-		AntennaConfig getAntennaConfig() {
-		    AntennaConfig config;
+AntennaConfig MetadataFileReader::getAntennaConfig() {
+    AntennaConfig config;
 
-		    // TODO: getPhysicalAntennaMappings()
-		    AntennaInputPhysID arr[] = {{0, 'X'}, {0, 'Y'}, {1, 'X'}, {1, 'Y'},
-                                        {6, 'X'}, {6, 'Y'}, {7, 'X'}, {7, 'Y'}};
-		    config.antennaInputs.assign(arr, arr + 8);
-		    // TODO: getFrequencyChannelsUsed()
-		    config.frequencyChannels.insert({124, 125, 127, 128, 129});
+	// TODO: getPhysicalAntennaMappings()
+	AntennaInputPhysID arr[] = {{0, 'X'}, {0, 'Y'}, {1, 'X'}, {1, 'Y'},
+                                {6, 'X'}, {6, 'Y'}, {7, 'X'}, {7, 'Y'}};
+	config.antennaInputs.assign(arr, arr + 8);
 
-		    return config;
-	    }
-};
+	// TODO: getFrequencyChannelsUsed()
+    config.frequencyChannels.insert({124, 125, 127, 128, 129});
+
+	return config;
+}
--- src/MetadataFileReader.hpp
+++ src/MetadataFileReader.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
-class MetadataFileReader;
+#include "Common.hpp"
+
+class MetadataFileReader {
+	public:
+        AntennaConfig getAntennaConfig();
+};
