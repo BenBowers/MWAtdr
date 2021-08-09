b498eaa6c653724d33e991d718039bb6bcc29f60
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-09 15:47:19 +0800

    CP6-107 Removed code from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include "common.hpp"
-
-//pre declaration of output singnal wrigter function
-void outSignalWriter(std::int16_t inputData[], AppConfig observation);
 
 int main() {
 
-    std::int16_t test[5] = {1,1,1,1,1};
-    outSignalWriter(test,observation);
-
-}
-
-
-void outSignalWriter(std::int16_t inputData[], AppConfig observation){
-    
-
-    //typecasting intagers to strings for the purpose of creating the output file name
-    //TODO include an output directory
-    std::string sObsID = std::to_string(observation.observationID);
-    std::string sStartTime = std::to_string(observation.signalStartTime);
-
-    //creating the output file with the correct name 
-    std::ofstream myfile(sObsID + "_" + sStartTime + "_signalchain.bin",std::ios::out | std::ios::binary);
-    //if the file is unable to be created and opened then an error is made will be handled later
-    if(!myfile){
-        //TODO error hanndling
-        std::cout << "cannot create file" << std::endl;
-    }
-    //input of the data into the file for tsting only.
-    for(int i = 0; i<3; i++)
-        //TODO Actual data wrighting 
-        //as of now only test will be writen to the file to confirm that file creation is working correctly
-        myfile.write("test\n",sizeof(char[5]));
-        myfile.close();   
 }
