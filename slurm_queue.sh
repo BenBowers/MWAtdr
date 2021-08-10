8ce4049d0ba42624a5c5a6f0b95a66c202d3e0ab
Benjamin Bowers <benjamin.bowers@student.curtin.edu.au>
2021-08-10 13:05:27 +0800

    CP6-99 Updated SLURM script with appropriate information

--- slurm_queue.sh
+++ slurm_queue.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -l
 
-#SBATCH --account=projectID
-#SBATCH --job-name=####
-#SBATCH --ntasks=###
-#SBATCH --ntasks-per-node=##
+#SBATCH --partition=workq
+#SBATCH --account=mwavcs
+#SBATCH --job-name=mpi_test
+#SBATCH --ntasks=10
+#SBATCH --ntasks-per-node=10
 #SBATCH --time=00:01:00
 #SBATCH --export=none
 
 module load singularity
 
-export pawseyRepository= # Pawsey repo here
-export containerImage=$pawseyRepository/ # Singularity image
+export pawseyRepository=/astro/mwavcs/capstone/
+export containerImage=$pawseyRepository/mpi_test_latest.sif
 
-srun --export=all -n $SLURM_NTASKS singularity exec $containerImage # rest of singularity args
+srun --export=all -n $SLURM_NTASKS singularity exec $containerImage $ROOT/app/build/mpi_test
