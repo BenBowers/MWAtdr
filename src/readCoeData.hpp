93d2cbd1e39acac0eaab72eb5fcf691323a12093
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-10 21:48:57 +0800

    Removed header includes that where not nessesery

--- src/readCoeData.hpp
+++ src/readCoeData.hpp
@@ -1,9 +1,4 @@
 #pragma once
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <array>
-#include <string>
 //Reading the coeficent data function will only take a file name and return an intager array to the caller of the function.
 std::vector<float> readCoeData(std::string fileName);
