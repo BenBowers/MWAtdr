6027fa4dd06689a48e4fd877ab1111fbbc522f0c
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-10 13:15:33 +0800

    CP6-105 Updated function type,
    Added Cmake for the readInputFile
    Updated Header File to contain correct function inputs

--- src/readInputFile.cpp
+++ src/readInputFile.cpp
@@ -7,13 +7,13 @@
 
 //Main function for reading in of the data file takes the name of the directory that the data files are stored in
 //will read all files for a specific calculation into 1 complex vector array for signal processing
-auto readFile(char* fileName, ){   
+std::vector<std::complex<float> > readFile(char* fileName,int antenaInput){   
     //declaration of variables used to construct the complex number. In future will be set by reading in the file
     double real;
     double imaginary;
 
     //creating the vector/array of complex numbers represending
-    std::vector<std::complex<double> > datavalues;
+    std::vector<std::complex<float> > datavalues;
     
     //test values will read this from the file later
     real = 1;
@@ -21,8 +21,7 @@ auto readFile(char* fileName, ){
     //filling the vectore with 65000 complex numbers with 1 being the real and imag part as a place holder
     for(int i = 0; i<= 65000;i++){
         //TODO FIle IO component will be included here
-        
-         std::vector<std::complex<double> > temp(real, imaginary);
+        std::complex<float> temp(real, imaginary);
         //depending on data files this might need to be chaged to push_fowrd
         datavalues.push_back(temp);
     }
