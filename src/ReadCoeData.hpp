ff4a2aab89c38510353128323a02499976905d62
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-11 14:50:27 +0800

    Updated header includes

--- src/ReadCoeData.hpp
+++ src/ReadCoeData.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
+#include <string>
 
 //Reading the coeficent data function will only take a file name and return an intager array to the caller of the function.
 std::vector<float> readCoeData(std::string fileName);
