224edc00b7a3141440e4ab7f934de1b4ec22c718
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-11 15:34:16 +0800

    Updated function names to more inline with recomendations

--- src/ReadInputFile.hpp
+++ src/ReadInputFile.hpp
@@ -4,7 +4,7 @@
 #include <complex>
 #include <string>
 //take s a file name will read that file remove the data that is applicable for this run of the program and output a set containing the data
-std::vector<std::complex<float> > readFile(std::string fileName,int antenaInput);
+std::vector<std::complex<float> > readInputDataFile(std::string fileName,int antenaInput);
 
 // takes in the file name this is a string will check to see if it is a standered pre defined size if this is found to be correct the function will return true if not it will return false
-bool validateData(std::string fileName);
+bool validateInputData(std::string fileName);
