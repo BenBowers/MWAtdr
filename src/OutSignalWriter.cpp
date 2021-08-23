e095ed75109585fb3b0a9d6c113fcab83e0d750d
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-23 14:11:11 +0800

    CP6-107 re added changes from deleted local branch
    Added file writing to directory from input
    Added writing data to file from vector

--- src/OutSignalWriter.cpp
+++ src/OutSignalWriter.cpp
@@ -13,19 +13,20 @@ void outSignalWriter(const std::vector<std::int16_t> &inputData, const AppConfig
     //TODO include an output directory
     std::string sObsID = std::to_string(observation.observationID);
     std::string sStartTime = std::to_string(observation.signalStartTime);
+    std::string sPhysID = std::to_string(physID.tile);
     std::string outDir = observation.outputDirectoryPath;
 
-    //creating the output file with the correct name 
-    std::ofstream myfile(sObsID + "_" + sStartTime + "_signalchain.bin",std::ios::out | std::ios::binary);
+    //creating the output file with the correct name and in the correct directory
+    std::ofstream myfile(outDir + sObsID + "_" + sStartTime + + "_signalchain.bin",std::ios::out | std::ios::binary);
     //if the file is unable to be created and opened then an error is made will be handled later
     if(!myfile){
         //TODO error hanndling
         std::cout << "cannot create file" << std::endl;
     }
-    //input of the data into the file for tsting only.
-    for(int i = 0; i<3; i++)
-        //TODO Actual data wrighting 
-        //as of now only test will be writen to the file to confirm that file creation is working correctly
-        myfile.write("test\n",sizeof(char[5]));
-        myfile.close();   
+
+    //dump the whole vector into a file
+    myfile.write(reinterpret_cast<const char*>(inputData.data()), sizeof(std::int16_t) * inputData.size());
+    myfile.close();
+
+
 }
