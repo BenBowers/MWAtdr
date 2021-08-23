fc251a27e2cb32bd06f8ac6dfa9802cc3164a390
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-23 14:58:55 +0800

    CP6-107 Creation of Unit tests for the outputfile Writer Module

---
+++ test/local/OutSignalWriterTest.cpp
@@ -0,0 +1,48 @@
+#include "../../src/OutSignalWriter.hpp"
+#include "../TestHelper.hpp"
+
+#include <optinal>
+#include <stdexcept>
+#include <vector>
+
+OutSignalWriterTest::OutSignalWriterTest() : TestModule{"Output File Writer unit test",{
+    
+    {"Invalid Inputs (Unexpected Data types)", []() {
+
+		try {
+
+			outSignalWriter(0,0,0);
+
+		    failTest();
+
+		}
+
+		catch (std::invalid_argument const&) {}
+
+	}},
+
+    {"Invalid inputs (incorect formatting)", []() {
+
+		try {
+
+			outSignalWriter(2,0,0,0);
+
+			failTest();
+
+		}
+
+		catch (std::invalid_argument const&) {}
+
+	}},
+    
+    {"Invalid output directory",[](){
+        
+        try {
+            outSignalWriter();
+            failTest();
+        }
+        catch(const std::exception& e){}
+
+    }}    
+
+}} {}
---
+++ test/local/OutSignalWriterTest.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../TestHelper.hpp"
+
+//unit test for the output file writer module
+class OutSignalWriterTest : public TestModule{
+    public:
+        OutSignalWriterTest();
+};
