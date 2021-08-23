89da3ded86db8061f362516e61d2266b0988c9b8
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-23 12:28:59 +0800

    CP6-109 Added console logs for testing

--- src/ReadCoeData.cpp
+++ src/ReadCoeData.cpp
@@ -26,16 +26,21 @@ std::vector<std::complex<float>> readCoeData(std::string fileName){
 
    
    //obtaining the filter length
+    std::clog << "Reading filter length" <<std::endl;    
     std::uint8_t filterLength;
     infile.read(reinterpret_cast<char *>(&filterLength),sizeof(filterLength));//first read get the size of the filter exactly 8 bits 
     //TODO error handeling with the filter size infomation
+    std::clog << "Completed filter length reading" <<std::endl;    
     
+    //creating buffer to hold coeficent data as it is read befor pushing it onto the vector
     float buffer;
+    std::clog << "Reading data values from file" <<std::endl;        
     while(!infile.eof()){
     infile.read(reinterpret_cast<char *>(&buffer),sizeof(buffer));//second read get the float data from the file
     result.push_back(buffer);
 
     }
+    std::clog << "Completed reading data values total values read"+result.size() <<std::endl;        
 
     infile.close();
     return result;
