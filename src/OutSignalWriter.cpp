06e62b57ebb7c4b29433236c95997cadb801a4a8
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-26 12:04:56 +0800

    CP6-111 Test cases

--- src/OutSignalWriter.cpp
+++ src/OutSignalWriter.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <iostream>
+#include <filesystem>
 #include "Common.hpp"
 #include "OutSignalWriter.hpp"
 
@@ -10,22 +11,27 @@ void outSignalWriter(const std::vector<std::int16_t> &inputData, const AppConfig
     
 
     //typecasting intagers to strings for the purpose of creating the output file name
-    //TODO include an output directory
+    //could possibly all be done inside the ofstream but for ease of use has been done outside
     std::string sObsID = std::to_string(observation.observationID);
     std::string sStartTime = std::to_string(observation.signalStartTime);
     std::string sPhysID = std::to_string(physID.tile);
-    std::string outDir = observation.outputDirectoryPath;
-
-    //creating the output file with the correct name and in the correct directory
-    std::ofstream outfile(outDir + "/" + sObsID + "_" + sStartTime + + "_signalchain.bin",std::ios::out | std::ios::binary);
-    //if the file is unable to be created and opened then an error is made will be handled later
-    if(!outfile){
-        //TODO error hanndling
-        std::cout << "cannot create file" << std::endl;
-
+    
+    //error checking to make sure the file dosnt already exist
+    if(std::filesystem::exists(observation.outputDirectoryPath + "/" + sObsID + "_" + sStartTime + "_" + sPhysID + "_signalchain.bin") == true){
+        throw std::ios::failure("file already exists");
     }
-    //dump the whole vector into a file
-    outfile.write(reinterpret_cast<const char*>(inputData.data()), sizeof(std::int16_t) * inputData.size());
-    outfile.close();
+    //creating the output file with the correct name and in the correct directory
+    std::ofstream outfile(observation.outputDirectoryPath + "/" + sObsID + "_" + sStartTime + "_" + sPhysID + "_signalchain.bin",std::ios::out | std::ios::binary);
+    
+    if(outfile.is_open()){
+        //dump the whole vector into a file
+        outfile.write(reinterpret_cast<const char*>(inputData.data()), sizeof(std::int16_t) * inputData.size());
+        outfile.close();        
+    }
+    else{
+        throw std::ios::failure("Error creating file");
+    }
+
+
 
 }
--- test/local/OutSignalWriterTest.cpp
+++ test/local/OutSignalWriterTest.cpp
@@ -6,51 +6,64 @@
 #include <stdexcept>
 #include <cstdint>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <iostream>
+
+void outSignalWriter(const std::vector<std::int16_t> &inputData, 
+                     const AppConfig &observation, 
+                     const AntennaInputPhysID &physID);
 
 
-std::vector<std::int16_t> testData = {1,2,3,4,5,6,7,8,9};
 //input path, obs id, signal start time, poly path, output dir
 AppConfig invalidTestConfig = {"123456789",123456789,123456789,"",""};
-AppConfig validTestConfig = {"",123456789,123456789,"",""}; //input and output directories are listed 
+AppConfig validTestConfig = {"",123456789,123456789,"","/app/test"}; //input and output directories are listed 
 //Antena input Physical ID the two components of that struct is the first physical id the second is the char representing what signal chain that id is for 
 AntennaInputPhysID testAntenaPhysID = {1,'x'};
+std::string filename = validTestConfig.outputDirectoryPath + "/" + std::to_string(validTestConfig.observationID) + "_" + std::to_string(validTestConfig.signalStartTime) + "_" + std::to_string(testAntenaPhysID.tile) + "_signalchain.bin";
 
 
 OutSignalWriterTest::OutSignalWriterTest() : TestModule{"Output File Writer unit test", {
-    
-    {"Verify output Data", []() {
-
-		try {
-			outSignalWriter(testData,validTestConfig,testAntenaPhysID);
-
-		    failTest();
-
-		}
-
-		catch (std::invalid_argument const&) {}
-
-	}},
-
-    {"Invalid inputs (incorrect output directory)", []() {
-
-		try {
-			outSignalWriter(testData,invalidTestConfig,testAntenaPhysID);
-
-			failTest();
-		}
-
-		catch (std::invalid_argument const&) {}
-
-	}},
-    
-    {"Invalid ",[](){
+    {"Valid Appconfig", []() {
+        std::vector<std::int16_t> testData = {1,2,3,4,5,6,7,8,9};
         
+        outSignalWriter(testData,validTestConfig,testAntenaPhysID);
+        
+        auto actual = std::filesystem::file_size(filename);
+        int expected = testData.size() * sizeof(std::int16_t);
+        testAssert(actual == expected);
+        
+        std::filesystem::remove(filename);
+	}},     
+    
+    {"Invalid Appconfig", []() {
         try {
-            outSignalWriter();
+            std::vector<std::int16_t> testData = {1,2,3,4,5,6,7,8,9};
+            outSignalWriter(testData,invalidTestConfig,testAntenaPhysID);
             failTest();
         }
-        catch(const std::exception& e){}
-
-    }}    
+        catch (std::ios::failure const&) {}        
+	}},
+    
+    {"Empty Data input", []() {
+        std::vector<std::int16_t> emptyTestData = {};
+        outSignalWriter(emptyTestData,validTestConfig,testAntenaPhysID);
+        auto actual = std::filesystem::file_size(filename);
+        int expected = emptyTestData.size() * sizeof(std::int16_t);
+        testAssert(actual == expected);
+        std::filesystem::remove(filename);
+	}},
+    
+    {"File already exists (double call to writer function)", []() {
+        try {
+            std::vector<std::int16_t> testData = {1,2,3,4,5,6,7,8,9};
+            outSignalWriter(testData,invalidTestConfig,testAntenaPhysID);
+            outSignalWriter(testData,invalidTestConfig,testAntenaPhysID);
+            failTest();
+        }
+        catch (std::ios::failure const&) {}
+        std::filesystem::remove(filename);        
+	}}            
 
 }} {}
