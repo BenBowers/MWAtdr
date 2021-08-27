74023af93e7758a50c6b5fbe8b9cc71ed6efa165
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-27 10:00:29 +0800

    Uppdated header to include Exceptions that are expected from the function

--- src/ReadCoeData.hpp
+++ src/ReadCoeData.hpp
@@ -3,5 +3,7 @@
 #include <complex>
 #include <string>
 
+//This Function will throw the following exceptions std::ios::failure 
+
 //Reading the coeficent data function will only take a file name and return an intager array to the caller of the function.
 std::vector<std::complex<float>> readCoeData(std::string fileName);
