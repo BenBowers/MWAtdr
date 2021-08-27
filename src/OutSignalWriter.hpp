ab4c019dc968164056b10608066bd20cc9c55545
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-27 10:05:50 +0800

    CP6-111 Updated header to include exceptions that are to be thrown

--- src/OutSignalWriter.hpp
+++ src/OutSignalWriter.hpp
@@ -7,5 +7,7 @@
 struct AppConfig;
 struct AntennaInputPhysID;
 
+//this fuction throws the following exception throw std::ios::failure
+
 
 void outSignalWriter(const std::vector<std::int16_t> &inputData, const AppConfig &observation, const AntennaInputPhysID &physID);
