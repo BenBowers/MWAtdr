4c89d8a206bc8cbc86b41ad92fe2667b8c249c57
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-08-29 20:37:39 +0800

    Readded seed test to main

--- test/local/Main.cpp
+++ test/local/Main.cpp
@@ -1,9 +1,13 @@
+#include <iostream>
+
 #include "../TestHelper.hpp"
 #include "OutSignalWriterTest.hpp"
 #include "ChannelRemappingTest.hpp"
 #include "NodeAntennaInputAssignerTest.hpp"
 
 int main() {
+    std::cout << "Test random number generator seed: " << seedTestRandomEngine() << '\n' << std::endl;
+    
     runTests({
         ChannelRemappingTest{},
         OutSignalWriterTest{},
