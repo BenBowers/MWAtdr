ab6498c599b9a067a2e1a21716aba0403aebfc59
Reece Jones <reece.w.jones@student.curtin.edu.au>
2021-08-31 14:27:35 +0800

    CP6-90 CP6-92 Finished channel remapper after updates from Clancy.
    Clancy has confirmed the issue with the channel aliasing and we have decided that the remapping will be left as-is, with corrections being made in the signal processing module.
    I also added some more test cases.

--- test/local/ChannelRemappingTest.hpp
+++ test/local/ChannelRemappingTest.hpp
@@ -3,7 +3,7 @@
 #include "../TestHelper.hpp"
 
 
-// Unit test for the frequency channel remapper module (ChannelRemapper.hpp and ChannelRemapper.cpp).
+// Unit test for the frequency channel remapper module (ChannelRemapping.hpp and ChannelRemapping.cpp).
 class ChannelRemappingTest : public TestModule {
 public:
     ChannelRemappingTest();
