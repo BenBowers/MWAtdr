bd4a81898694de46d72e1b6d5f5cdc19eff8a4e1
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-09-01 10:39:10 +0800

    Updated exception comment in header file

--- src/OutSignalWriter.hpp
+++ src/OutSignalWriter.hpp
@@ -8,8 +8,7 @@
 struct AppConfig;
 struct AntennaInputPhysID;
 
-//this fuction throws the following exception throw std::ios_base::failure for IO related failures during runtime
-// std::system_error::runtime_error is used for any non IO related errors that are encountered when consturction file strings
+//This function throws the following eception outSignalExcpetion that can be caught and ther error string being used with e.getMessage() to get the error message from the exception object
 
 void outSignalWriter(const std::vector<std::int16_t> &inputData, const AppConfig &observation, const AntennaInputPhysID &physID);
 
