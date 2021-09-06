9a41dfca78d84a85d7ffaa04afb539b83bfef0b7
Reece Jones <reece.w.jones@student.curtin.edu.au>
2021-09-06 19:15:17 +0800

    CP6-126 Enabled compile tuning for Garrawarla.
    Added a RUNTIME_SYSTEM Docker build argument, which specifies what system to build for.

--- docker_build.ps1
+++ docker_build.ps1
@@ -4,6 +4,8 @@ param(
     [Parameter(Mandatory=$True, Position = 1)]
     [String] $buildType,
     [Parameter(Mandatory=$True, Position = 2)]
+    [String] $runtimeSystem,
+    [Parameter(Mandatory=$True, Position = 3)]
     [String] $containerRuntime
 )
 
@@ -11,6 +13,7 @@ docker build `
     --target "$target" `
     -t "mwa_time_data_reconstructor/$target" `
     --build-arg "BUILD_TYPE=$buildType" `
+    --build-arg "RUNTIME_SYSTEM=$runtimeSystem" `
     --build-arg "CONTAINER_RUNTIME=$containerRuntime" `
     --progress plain `
     .
