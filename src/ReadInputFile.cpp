49db5490b5298c179d4778dfa18c51fda1e84af1
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-09-11 10:47:33 +0800

    Updates

--- src/ReadInputFile.cpp
+++ src/ReadInputFile.cpp
@@ -10,36 +10,34 @@
 //Main function for reading in of the data file takes the name of the directory that the data files are stored in
 //will read all files for a specific calculation into 1 complex vector array for signal processing
 std::vector<std::complex<float> > readInputDataFile(std::string fileName,int antenaInput){   
-    //declaration of variables used to construct the complex number. In future will be set by reading in the file
-    double real;
-    double imaginary;
-
-    //creating the vector/array of complex numbers represending
     std::vector<std::complex<float> > datavalues;
-    
-    //test values will read this from the file later
-    real = 1;
-    imaginary = 1;
-    //filling the vectore with 65000 complex numbers with 1 being the real and imag part as a place holder
-    for(int i = 0; i<= 65000;i++){
-        //TODO FIle IO component will be included here
-        std::complex<float> temp(real, imaginary);
-        //depending on data files this might need to be chaged to push_fowrd
-        datavalues.push_back(temp);
+    std::ifstream datafile(fileName, std::ios::binary);
+    // main error handling statement
+    if(datafile.is_open()){
+        //error checking to make sure the file is of the right size this is to validate that all the infomation inside atleast of the correct
+
+
+        // reading the data into the array
+       datavalues.reserve(65000*sizeof(std::int8_t));
+       while(datafile.read(reinterpret_cast<char*>(&rbuffer), sizeof(float)))
+
+       datavalues.push_back({rbuffer,0.0f});
     }
-    return datavalues;
+    else{
+        throw ReadInputDataException("Failed to open the file");
+    }
+
 }
 
 
 //function used to validate if the data file is the correct size and thus allowing the program to know if there is anything missing.
-//all data files will be 1 specific size so it will be assumed that any file that is not of the correct size is malfored and not usable for calculations.
-//it is going to be assumed for testing purposes that all files are correct.
-//TODO Actual file validation
+//The file size this program will be given is a constant as such its easy to validate if the file is correct or not
+//break
 bool validateInputData(std::string fileName){
-    return true;
-}
-
-std::vector<std::complex<float>> makedatavalues(){
-
-    
+    if(std::filesystem::file_size(fileName) != 1+filterSize*filterLength*sizeof(float)){
+        throw ReadInputDataException("Error File size is not expected");
+    }    
+    else{
+        return true;
+    }
 }
