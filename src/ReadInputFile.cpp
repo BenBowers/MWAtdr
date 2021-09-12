aa84fc76954cf68bc6e8bf347f5c59532ecb4449
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-09-12 11:58:36 +0800

    Added file offsets to reach the specific data inside the file

--- src/ReadInputFile.cpp
+++ src/ReadInputFile.cpp
@@ -1,4 +1,5 @@
 #include "ReadInputFile.hpp"
+#include "Common.hpp"
 #include <complex>
 #include <iostream>
 #include <fstream>
@@ -6,22 +7,34 @@
 #include <string>
 #include <filesystem>
 
+//where dose this random ass number come from
+//MWA documentation states this is the standered file size for a 128 tile dule polarisation file
+long stdinputsize = 5275652096;
 
 //Main function for reading in of the data file takes the name of the directory that the data files are stored in
 //will read all files for a specific calculation into 1 complex vector array for signal processing
 std::vector<std::complex<float> > readInputDataFile(std::string fileName,int antenaInput){   
     std::vector<std::complex<float> > datavalues;
     std::ifstream datafile(fileName, std::ios::binary);
+    
+    //this gets changed depending on what antean we want to read the data from
+    int offset = 0;
     // main error handling statement
     if(datafile.is_open()){
+
         //error checking to make sure the file is of the right size this is to validate that all the infomation inside atleast of the correct
 
+        //reading the data into the vector
 
-        // reading the data into the array
-       datavalues.reserve(65000*sizeof(std::int8_t));
-       while(datafile.read(reinterpret_cast<char*>(&rbuffer), sizeof(float)))
-
-       datavalues.push_back({rbuffer,0.0f});
+        datavalues.reserve(64000*sizeof(std::int8_t));
+        std::int8_t rbuffer;
+        std::int8_t ibuffer;
+        //alot of this is dependent on the meta data file reader numbers are subject to change once i figure out what to do
+        //seeking to the start of the data portion of the file 
+        //this will be antena 0 polarisation x sample 1 of 64000
+        datafile.seekg(4096+32768000+offset, std::ios::beg);
+        while(datafile.read(reinterpret_cast<char*>(&rbuffer), 64000*sizeof(std::int8_t)))
+        datavalues.push_back({rbuffer,ibuffer});
     }
     else{
         throw ReadInputDataException("Failed to open the file");
@@ -34,7 +47,7 @@ std::vector<std::complex<float> > readInputDataFile(std::string fileName,int ant
 //The file size this program will be given is a constant as such its easy to validate if the file is correct or not
 //break
 bool validateInputData(std::string fileName){
-    if(std::filesystem::file_size(fileName) != 1+filterSize*filterLength*sizeof(float)){
+    if(std::filesystem::file_size(fileName) != stdinputsize){
         throw ReadInputDataException("Error File size is not expected");
     }    
     else{
