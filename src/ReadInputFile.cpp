cbdb7394fcd7fb392a99a33ad8ebe388325a3d9a
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-09-16 19:57:50 +0800

    Testing to see if there is a beter option for handling the x-y polarisation for file reading and function calling

--- src/ReadInputFile.cpp
+++ src/ReadInputFile.cpp
@@ -9,11 +9,14 @@
 
 //where dose this random ass number come from
 //MWA documentation states this is the standered file size for a 128 tile dual polarisation file
+//4096+161*32768000
+//meta data 160 volatage data blocks + the single delay block before the data * the size of the delay block what is 
+//Number of tiles *2 polarisations * 128000 bytes 64000 1 byte samples for each real and imag part
 const long long stdinputsize = 5275652096;
 
 //Main function for reading in of the data file takes the name of the file it is to read from
 //will read all files for a specific calculation into 1 complex vector array for signal processing
-std::vector<std::vector<std::complex<float>>> readInputDataFile(std::string fileName,int antenaInput){    
+std::vector<std::vector<std::complex<float>>> readInputDataFile(std::string fileName,int antenaInput, char pol){    
     //this is the number of samples per 50ms time slice in the data files this is subject to change based on the MWA wiki
     const int NUMSAMPLES = 64000;
     //Meta data buffer size inside the data files
@@ -46,10 +49,18 @@ std::vector<std::vector<std::complex<float>>> readInputDataFile(std::string file
             //per antena per polarisation there is a 64000 bytes of data        
             long long offset;
             if(antenaInput != 0){   
-                offset = NUMSAMPLES*antenaInput;
+                if(pol == 'x'){
+                    offset = NUMSAMPLES*antenaInput;
+                }
+                //assuming pol y
+                else{offset = NUMSAMPLES*antenaInput +128000;}
+                
             }
             else{
-                offset = 0;
+                if(pol == 'x'){
+                    offset = 0;
+                } else{offset = 128000;}
+                
             }        
             //reading the data into the vector
             //known size of data file enteries as per file specification pre allocation to save time later
@@ -59,7 +70,7 @@ std::vector<std::vector<std::complex<float>>> readInputDataFile(std::string file
             //this will be antena 0 polarisation x and y sample 1 of 64000
             datafile.seekg(METADATASIZE+offset, std::ios::beg);
             for(int i = 1; i <= 160;i++){
-                datafile.seekg(32768000, std::ios::cur);
+                datafile.seekg(DELAYDATALENGTH, std::ios::cur);
                 for(int j = 1; j<=NUMSAMPLES;j++){
                     signed char rbuffer;
                     signed char ibuffer;
--- test/local/ReadInputFileTest.cpp
+++ test/local/ReadInputFileTest.cpp
@@ -20,7 +20,7 @@ std::vector<TestCase> ReadInputFileTest::getTestCases() {
     return {        
         {"Valid Data File", []() {
             try{
-                auto data = readInputDataFile("1294797712_1294797712",0);
+                auto data = readInputDataFile("1294797712_1294797712",0,'x');
                 testAssert(data.size() == 4); 
             }
             catch(ReadInputDataException const& e){}
