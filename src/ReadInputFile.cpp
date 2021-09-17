668a278a5d7f66efd4ed6420ae83abe3e79efe27
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-09-17 22:46:06 +0800

    Local file meta data reader

--- src/ReadInputFile.cpp
+++ src/ReadInputFile.cpp
@@ -13,14 +13,16 @@
 //meta data 160 volatage data blocks + the single delay block before the data * the size of the delay block what is 
 //Number of tiles *2 polarisations * 128000 bytes 64000 1 byte samples for each real and imag part
 const long long stdinputsize = 5275652096;
+//Meta data buffer size inside the data files constaining a subset of the overall meta data
+const int METADATASIZE = 4096;
 
+int getNinputs(std::string fileName);
 //Main function for reading in of the data file takes the name of the file it is to read from
 //will read all files for a specific calculation into 1 complex vector array for signal processing
 std::vector<std::vector<std::complex<float>>> readInputDataFile(std::string fileName,int antenaInput){    
     //This is the number of samples per 50ms time slice in the data files this is subject to change based on the MWA wiki
     const int NUMSAMPLES = 64000;
-    //Meta data buffer size inside the data files constaining a subset of the overall meta data
-    const int METADATASIZE = 4096;
+
     //This needs to be moved to a diff function as for each file it could be a different number
     const long long NUMTILES = 128;
     //This is how large the delay meta data block is inside of the file is is dependent on how many tiles are in the observation
@@ -34,6 +36,8 @@ std::vector<std::vector<std::complex<float>>> readInputDataFile(std::string file
             allfiles.push_back(curpath);
         }    
     }
+
+    getNinputs("signals/1294797712_1294797712_1.sub");
     
     std::vector<std::vector<std::complex<float>>> datavalues;
     for (int k = 0; k < allfiles.size(); k++){
@@ -101,3 +105,29 @@ bool validateInputData(std::string fileName){
         return true;
     }
 }
+
+int getNinputs(std::string fileName){
+    std::ifstream f(fileName);
+    int nPols;
+    if (f){
+        f.seekg(4096, std::ios::beg);
+        const auto size = f.tellg();
+        std::string str(size, ' ');
+        f.seekg(0);
+        f.read(&str[0], size); 
+        f.close();
+        //std::cout << str << std::endl;
+        int pos = str.find("NPOL");
+
+        std::string sNpols = str.substr(pos+5,2); 
+        nPols = std::stoi(sNpols);
+        std::cout << nPols <<std::endl;
+    }
+    return nPols;
+}
+
+int getNsamples(std::string fileName){
+
+
+}
+
