3ec8fde9f97dfa75a35c1d0c27ed8227856c5281
Dylan Chanter <dylan.chanter@student.curtin.edu.au>
2021-09-20 16:52:28 +0800

    Code refactor to only read the meta data once from the file header

--- src/ReadInputFile.cpp
+++ src/ReadInputFile.cpp
@@ -14,6 +14,7 @@
 #include <filesystem>
 
 //Meta data inside each file readers
+std::string getMetaDataString(std::string fileName);
 int getMetaDataSize(std::string fileName);
 int getNinputs(std::string fileName);
 int getNpols(std::string fileName);
@@ -36,11 +37,13 @@ std::vector<std::vector<std::complex<float>>> readInputDataFile(std::string file
 
     std::vector<std::vector<std::complex<float>>> datavalues;
     for (int k = 0; k < allfiles.size(); k++){
-        int metadatasize = getMetaDataSize(allfiles.at(k));
+        
+        std::string metadata = getMetaDataString(allfiles.at(k));
+        int metadatasize = getMetaDataSize(metadata);
         //This is the number of samples per 50ms time slice in the data files this is subject to change based on the MWA wiki
-        int NUMSAMPLES = getNsamples(allfiles.at(k));
+        int NUMSAMPLES = getNsamples(metadata);
         //This needs to be moved to a diff function as for each file it could be a different number
-        long long NUMTILES = getNinputs(allfiles.at(k)) * getNpols(allfiles.at(k));
+        long long NUMTILES = getNinputs(metadata) * getNpols(metadata);
         //This is how large the delay meta data block is inside of the file is is dependent on how many tiles are in the observation
         long long DELAYDATALENGTH = NUMTILES*128000;
 
@@ -96,9 +99,11 @@ bool validateInputData(std::string fileName){
     //4096+161*32768000
     //meta data 160 volatage data blocks + the single delay block before the data * the size of the delay block what is 
     //Number of tiles *2 polarisations * 128000 bytes 64000 1 byte samples for each real and imag part
-    long samplebytesize = getNsamples(fileName)*2;
-    long delaydata = getNinputs(fileName) * getNpols(fileName) * samplebytesize;
-    long metadatasize = getMetaDataSize(fileName);
+    std::string metadata = getMetaDataString(fileName);    
+    //2 bytes per sample
+    long samplebytesize = getNsamples(metadata)*2;
+    long delaydata = getNinputs(metadata) * getNpols(metadata) * samplebytesize;
+    long metadatasize = getMetaDataSize(metadata);
     long long expectedInputSize = metadatasize + delaydata * 161;
     if(std::filesystem::file_size(fileName) != expectedInputSize){
         throw ReadInputDataException("Error File size is not expected");
@@ -108,9 +113,8 @@ bool validateInputData(std::string fileName){
     }
 }
 
-int getNpols(std::string fileName){
-    std::ifstream f(fileName);
-    int nPols;
+std::string getMetaDataString(std::string fileName){
+    std::ifstream f(fileName);   
     if (f){
         f.seekg(4096, std::ios::beg);
         const auto size = f.tellg();
@@ -118,79 +122,48 @@ int getNpols(std::string fileName){
         f.seekg(0);
         f.read(&str[0], size); 
         f.close();
-        int pos = str.find("NPOL");
-        int delim = str.find("\n",pos);
-        std::string sNpols = str.substr(pos+5,delim); 
-        nPols = std::stoi(sNpols);
+        return str;
     }
     else{
         throw ReadInputDataException("Error Reading meta data from file");
-    }
+    }    
+}
+
+int getNpols(std::string str){
+    int nPols;
+    int pos = str.find("NPOL");
+    int delim = str.find("\n",pos);
+    std::string sNpols = str.substr(pos+5,delim); 
+    nPols = std::stoi(sNpols);
     return nPols;
 }
 
 //returns the number of samples per 50ms time slice in the data file 
-int getNsamples(std::string fileName){
-    std::ifstream f(fileName);
+int getNsamples(std::string str){
     int nSamples;
-    if (f){
-        f.seekg(4096, std::ios::beg);
-        const auto size = f.tellg();
-        std::string str(size, ' ');
-        f.seekg(0);
-        f.read(&str[0], size); 
-        f.close();
-        int pos = str.find("NTIMESAMPLES");
-        int delim = str.find("\n",pos);
-        std::string snSamples = str.substr(pos+13,delim); 
-        nSamples = std::stoi(snSamples);
-    }
-    else{
-        throw ReadInputDataException("Error Reading meta data from file");
-    }    
+    int pos = str.find("NTIMESAMPLES");
+    int delim = str.find("\n",pos);
+    std::string snSamples = str.substr(pos+13,delim); 
+    nSamples = std::stoi(snSamples);  
     return nSamples;
 }
 
 //returns the humber of fine channels that the meta data expects in the file
-int getNinputs(std::string fileName){
-    std::ifstream f(fileName);
+int getNinputs(std::string str){
     int nInputs;
-    if (f){
-        f.seekg(4096, std::ios::beg);
-        const auto size = f.tellg();
-        std::string str(size, ' ');
-        f.seekg(0);
-        f.read(&str[0], size); 
-        f.close();
-        int pos = str.find("NFINE_CHAN");
-        int delim = str.find("\n",pos);        
-        std::string snInputs = str.substr(pos+10,delim); 
-        nInputs = std::stoi(snInputs);
-    }
-    else{
-        throw ReadInputDataException("Error Reading meta data from file");
-    }    
+    int pos = str.find("NFINE_CHAN");
+    int delim = str.find("\n",pos);        
+    std::string snInputs = str.substr(pos+10,delim); 
+    nInputs = std::stoi(snInputs); 
     return nInputs;
 }
 
 //will return the size of the meta data header block described in the first line of the file
-int getMetaDataSize(std::string fileName){
-    std::ifstream f(fileName);
-    int nPols;
-    if (f){
-        f.seekg(300, std::ios::beg);
-        const auto size = f.tellg();
-        std::string str(size, ' ');
-        f.seekg(0);
-        f.read(&str[0], size); 
-        f.close();
-        int pos = str.find("HDR_SIZE");
-        int delim = str.find("\n",pos);
-        std::string sHRDsize = str.substr(pos+8,delim); 
-        nPols = std::stoi(sHRDsize);
-    }
-    else{
-        throw ReadInputDataException("Error Reading meta data from file");
-    }
-    return nPols;
+int getMetaDataSize(std::string str){
+    int mSize;
+    int pos = str.find("HDR_SIZE");
+    int delim = str.find("\n",pos);
+    std::string sHRDsize = str.substr(pos+8,delim); 
+    mSize = std::stoi(sHRDsize);
+    return mSize;
 }
