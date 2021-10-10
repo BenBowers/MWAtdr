6ea8cdd2413d1efce15ee2647041606027bc662d
DylanChanter <dylan.chanter@student.curtin.edu.au>
2021-10-11 00:29:30 +0800

    inital commit

--- test/integration/test_main.py
+++ test/integration/test_main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-
 from helpers import run_application
+import os
+import sys
 
 
 def test_dummy(run_script: Path, working_dir: Path) -> None:
@@ -9,3 +10,17 @@ def test_dummy(run_script: Path, working_dir: Path) -> None:
     output_dir = working_dir / 'output_dir'
     result = run_application(run_script, input_dir, '12345678', '12345678', inverse_polyphase_filter_file, output_dir, 'true')
     assert result.returncode == 0
+
+
+def test_case_2()
+    input_dir = working_dir / 'input_data'
+    
+    
+    file = open(working_dir / "inverse_polyphase_filter.bin", "wb")
+    for i in range(1,256): 
+        file.write(b"This binary string will be written to sample.bin")
+    file.close()
+    
+    inverse_polyphase_filter_file = working_dir / 'inverse_polyphase_filter.bin'
+    output_dir = working_dir / 'output_dir'
+    result = run_application(run_script, input_dir, '12345678', '12345678', inverse_polyphase_filter_file, output_dir, 'true')
