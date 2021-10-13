76e0bfc8fb53dd443ac9185396370bf15d6b6553
Zak Booth <zak.booth@student.curtin.edu.au>
2021-10-13 21:12:20 +0800

    CP6-139 Made integration tests for no signal data but valid metafits

--- test/integration/test_main.py
+++ test/integration/test_main.py
@@ -3,9 +3,47 @@ from pathlib import Path
 from helpers import run_application
 
 
-def test_dummy(run_script: Path, working_dir: Path) -> None:
+def test_no_data_ignore_errors(run_script: Path, working_dir: Path) -> None:
+    # Test with no signal data, but a valid metafits and coefficients filter
+
+    working_dir = working_dir / 'no_data'
+    working_dir.mkdir(exist_ok=False, parents=True)
+
+    inv_polyphase_filter_path = working_dir / 'inverse_polyphase_filter.bin'
+    inv_polyphase_filter = numpy.zeros((13, 256), dtype=numpy.float32)
+    inv_polyphase_filter[:, -1] = 1      # Identity
+    write_inv_polyphase_filter(inv_polyphase_filter_path, inv_polyphase_filter)
+
     input_dir = working_dir / 'input_data'
-    inverse_polyphase_filter_file = working_dir / 'inverse_polyphase_filter.bin'
+    input_dir.mkdir(exist_ok=False, parents=True)
+    shutil.copyfile(TEST_DATA_PATH / '1294797712.metafits', input_dir / '1294797712.metafits')
+
     output_dir = working_dir / 'output_dir'
-    result = run_application(run_script, input_dir, '12345678', '12345678', inverse_polyphase_filter_file, output_dir, 'true')
-    assert result.returncode == 0
+    output_dir.mkdir(exist_ok=False, parents=True)
+
+    result = run_application(run_script, input_dir, '1294797712', '1294797712', inv_polyphase_filter_path, output_dir, 'true')
+    
+    assert result.returncode == 78
+
+
+def test_no_data_acknowledge_errors(run_script: Path, working_dir: Path) -> None:
+    # Test with no signal data, but a valid metafits and coefficients filter
+
+    working_dir = working_dir / 'no_data'
+    working_dir.mkdir(exist_ok=False, parents=True)
+
+    inv_polyphase_filter_path = working_dir / 'inverse_polyphase_filter.bin'
+    inv_polyphase_filter = numpy.zeros((13, 256), dtype=numpy.float32)
+    inv_polyphase_filter[:, -1] = 1      # Identity
+    write_inv_polyphase_filter(inv_polyphase_filter_path, inv_polyphase_filter)
+
+    input_dir = working_dir / 'input_data'
+    input_dir.mkdir(exist_ok=False, parents=True)
+    shutil.copyfile(TEST_DATA_PATH / '1294797712.metafits', input_dir / '1294797712.metafits')
+
+    output_dir = working_dir / 'output_dir'
+    output_dir.mkdir(exist_ok=False, parents=True)
+
+    result = run_application(run_script, input_dir, '1294797712', '1294797712', inv_polyphase_filter_path, output_dir, 'false')
+    
+    assert result.returncode == 78
