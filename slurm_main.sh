5d7982749f5bd0c2e8f66b9744261f4525fe77a5
Benjamin Bowers <benjamin.bowers@student.curtin.edu.au>
2021-10-14 17:35:09 +0800

    Tested main slurm script

--- slurm_main.sh
+++ slurm_main.sh
@@ -1,10 +1,12 @@
-#!/bin/bash -l
+#!/bin/sbatch
 
 #SBATCH --partition=workq
 #SBATCH --account=mwavcs
-#SBATCH --job-name=local_test
-#SBATCH --nodes=1
-#SBATCH --time=00:01:00
+#SBATCH --job-name=mwatdr
+#SBATCH --ntasks=128
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=14G
+#SBATCH --time=00:15:00
 #SBATCH --export=none
 
 module load singularity-openmpi
@@ -14,15 +16,8 @@ if [[ $# -ne 6 ]] ; then
     exit 1
 fi
 
-hostInputDir=$(realpath -m $1)
-obsId=$2
-startTime=$3
-hostInvPolyphaseFilterFile=$(realpath -m $4)
-hostOutputDir=$(realpath -m $5)
-ignoreErrors=$6
 export pawseyRepository=/astro/mwavcs/capstone/
-export containerImage=$pawseyRepository/local-test.sif
-export repoDir = /astro/mwavcs/capstone/repo/
+export containerImage=$pawseyRepository/images/main.sif
 
 export hostInputDir=$(realpath -m $1)
 export obsId=$2
@@ -31,12 +26,10 @@ export hostInvPolyphaseFilterFile=$(realpath -m $4)
 export hostOutputDir=$(realpath -m $5)
 export ignoreErrors=$6
 
-export containerInputDir="/mnt/input_data"
-export containerInvPolyphaseFilterFile="/mnt/inverse_polyphase_filter"
-export containerOutputDir="/mnt/output_data"
+export containerInputDir=/mnt/input_data
+export containerInvPolyphaseFilterFile=/mnt/inverse_polyphase_filter
+export containerOutputDir=/mnt/output_data
 
-srun -n --export=all singularity exec --pwd=/app\
-     -B $hostInputDir:$containerInputDir:ro,\
-        $hostOutputDir:$containerOutputDir:rw,\
-        $hostInvPolyphaseFilterFile:$containerInvPolyphaseFilterFile:ro \
+srun --export=all -n $SLURM_NTASKS  singularity exec --pwd=/app \
+     --bind $hostInputDir:$containerInputDir:ro,$hostOutputDir:$containerOutputDir:rw,$hostInvPolyphaseFilterFile:$containerInvPolyphaseFilterFile:ro \
     $containerImage $ROOT/app/entrypoint.sh $containerInputDir $obsId $startTime $containerInvPolyphaseFilterFile $containerOutputDir $ignoreErrors
