8dd2576901cd71918df37f4e2266b4de1e774b64
Reece Jones <reece.w.jones@student.curtin.edu.au>
2021-10-15 12:31:31 +0800

    Minor final changes.
    Removed PowerShell scripts because client won't need those.
    Removed docker_run script because it doesn't run any target any more (development tool).
    Removed unused code from integration tests.

--- docker_run.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Mandatory=$True, Position = 0)]
-    [String] $target,
-
-    [Parameter(Mandatory=$False, Position = 1, ValueFromRemainingArguments=$true)]
-    [String[]] $arguments
-)
-
-docker run -t "mwatdr/$target" @arguments
